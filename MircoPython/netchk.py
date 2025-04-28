9efaaa6f611c3dd57f33d2527c0abed0290c5e71
cjstoddard <chris.j.stoddard@gmail.com>
2025-04-28 12:21:54 -0500

    modified:   MircoPython/netchk.py

--- MircoPython/netchk.py
+++ MircoPython/netchk.py
@@ -1,12 +1,19 @@
 import network
+import ntptime
 
 wlan = network.WLAN(network.STA_IF)  # STA_IF = station interface (client mode)
+UTF_OFFSET = -5 * 60 * 60 # Change -5 for your timezone
 
 if wlan.isconnected():
     ip_info = wlan.ifconfig()
     print("My IP address is:", ip_info[0])  # [0] is the IP address
+    ntptime.settime()
+    now = time.localtime(time.time() + UTF_OFFSET)
+    print("\n")
+    print("Date: {}/{}/{}".format(now[1], now[2], now[0]))
+    print("Time: {}:{}".format(now[3], now[4]))
 else:
     print("Not connected to WiFi.")
 
-print("SD Card Listing")
+print("\n Files on the SD Card.")
 print(os.listdir('/sd'))
